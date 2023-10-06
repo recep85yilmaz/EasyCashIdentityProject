f19d056538f386545233c7948c25dbe873264ac0
recep85yilmaz <recep85yilmaz@gmail.com>
2023-10-06 14:38:15 +0300

    #3 - Referans İşlemleri ve Entitylerin Yazılması

---
+++ EasyCashIdentityProject.EntityLayer/1ConcreteSomutDegerler/CustomerAccount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.EntityLayer._1ConcreteSomutDegerler
+{
+    public class CustomerAccount
+    {
+        public int CustomerAccountID { get; set; } //Sonuna ID gelince [key] eklemesine gerek kalmaz
+        public string CustomerAccountNumber { get; set; }
+        public string Currency { get; set; }
+        public decimal CustomerAccountBalance { get; set; }
+        public string BankBranch { get; set; }
+        
+
+
+    }
+}
